7f7f80504dcdbc45617105a4e04cf86df0ce7e49
Riccardo <riccardo.corradin@live.com>
2026-05-26 14:27:11 +0200

    feat: clickable places from map

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -57,33 +57,17 @@
                 features.map((feature) => {
                     if (feature.sourceLayer === "poi") {
                         console.log(feature);
+                        console.log(e.lngLat.lng);
+                        new maplibregl.Popup()
+                            .setLngLat(e.lngLat)
+                            .setHTML(
+                                `<h3 style="margin: 0 0 8px 0; font-weight: 600;">${feature.properties?.name}</h3>`,
+                            )
+                            .addTo(map);
                     }
                 });
             }
-            // const coordinates = e.features[0].geometry.coordinates.slice();
-            // const description = e.features[0].properties.description;
-            // // Ensure that if the map is zoomed out such that multiple
-            // // copies of the feature are visible, the popup appears
-            // // over the copy being pointed to.
-            // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            //     coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-            // }
-            // new maplibregl.Popup()
-            //     .setLngLat(coordinates)
-            //     .setHTML(description)
-            //     .addTo(map);
         });
-
-        // Change the cursor to a pointer when the mouse is over the places layer.
-        // map.on("mouseenter", "places", () => {
-        //     map.getCanvas().style.cursor = "pointer";
-        // });
-
-        // // Change it back to a pointer when it leaves.
-        // map.on("mouseleave", "places", () => {
-        //     map.getCanvas().style.cursor = "";
-        // });
-
         updateMarkers();
     });
 
